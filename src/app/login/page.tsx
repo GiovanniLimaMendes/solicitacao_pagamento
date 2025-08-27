f93618e5f0be417bfefab8db28d16e73dfa2a3ca
Suporte | CALIBRAS <suporte@equipartec.local>
2025-08-27 09:01:34 -0300

    Atualizado caminho da API de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -31,7 +31,7 @@ export default function Login() {
     setAlert(null);
 
     try {
-      const res = await fetch("http://localhost:3000/api/login", {
+      const res = await fetch("/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, senha }),
